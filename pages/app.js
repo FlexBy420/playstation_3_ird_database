48e736c530ab79e19f59ba19ea3a78463a3b8448
FlexBy420 <flexby420@gmail.com>
2024-04-13 01:22:12 +0200

    add a simple go to top button

--- pages/app.js
+++ pages/app.js
@@ -183,3 +183,16 @@ async function Init() {
 document.addEventListener('DOMContentLoaded', Init);
 window.addEventListener('load', Init);
 //# sourceMappingURL=app.js.map
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
